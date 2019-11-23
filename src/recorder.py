5e520b0b64de50bbed6f7862287d2b88f84dd72c
mer <imt.killian56@outlook.fr>
2019-11-23 15:48:24 +0100

    Add Docker config / Els & Kibana

--- src/recorder.py
+++ src/recorder.py
@@ -1,5 +1,6 @@
 import pyshark
 
+# Interface eth0 on VPS
 capture = pyshark.LiveCapture(interface='en0',  display_filter="tcp&&(ipv6.dst==2a01:e0a:455:7b0:dcd3:b238:3423:6cce||ip.dst==192.168.0.10)", output_file="./src/capture.pcap")
 
 capture.sniff(timeout=30)
